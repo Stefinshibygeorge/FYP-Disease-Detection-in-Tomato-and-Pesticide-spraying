1ab18b8d946ad0c2c735145821a86589346ecf8e
Ssg <105969983+Stefinshibygeorge@users.noreply.github.com>
2025-07-31 23:38:16 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,24 @@
 model_path = "/home/Ssg_K/fyp/tomato_model.h5"
 
+set_cause = 'v'
+
+
+
+
+
+# Disease labels
+disease_labels = [
+    "Bacterial Spot",
+    "Late Blight",
+    "Septoria Leaf Spot",
+    "Tomato Yellow Leaf Curl Virus",
+    "Tomato Mosaic Virus",
+    "Healthy"
+]
+
+
+
+
 # Constants
 target_size = (224, 224)  # Model expects 224x224 images
 green_threshold = 0.05
