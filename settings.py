3c12fb4abf6bb805a623fb325d2b3b14b36ab0b1
Ssg <105969983+Stefinshibygeorge@users.noreply.github.com>
2025-07-31 23:36:31 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,10 @@
 model_path = "/home/Ssg_K/fyp/tomato_model.h5"
+
+# Constants
+target_size = (224, 224)  # Model expects 224x224 images
+green_threshold = 0.05
+n_rows = 5
+n_cols = 5
+
+# car controls
+speed = 100
